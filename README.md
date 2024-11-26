803de3922f34c7140ed1af182f45bacf6fc25a24
Andres Goyes <andres.goyes@correounivalle.edu.co>
2024-11-26 10:00:06 -0500

    mejorando itinerarios

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
 # mytinerary-AndresGoyes
 Travel website built with the MERN stack (MongoDB, Express, React, Node.js). It provides an intuitive user experience for exploring destinations, booking trips, and managing itineraries. The project features dynamic front-end functionality with React, and a powerful back-end using Node.js, Express, and MongoDB for database management.
-
-***INSTALL DEPENDENCIES***
-- npm i
-
-***RUN FRONTEND***
-- npm run dev
--- src/Components/Itineraries.jsx
+++ src/Components/Itineraries.jsx
@@ -47,45 +47,76 @@ const Itineraries = () => {
     if (error) return <div><p className="text-gray-700 text-xl">No itineraries found for this city.</p></div>;
 
     return (
-        <main className="flex flex-col items-center justify-center py-4">
-            <section id="itineraries" className="space-y-10 text-neutral-500 dark:text-neutral-300 text-center">
-                <h1 className="text-5xl font-bold">ITINERARIES</h1>
-                <div className="flex flex-wrap justify-center gap-12">
+        <main className="flex flex-col items-center justify-center">
+            <section id="itineraries" className="text-neutral-500 dark:text-neutral-300 text-center">
+                <h1 className="text-4xl font-bold mb-12">ITINERARIES</h1>
+                <div className="grid justify-items-center gap-8">
                     {updatedItineraries.length > 0 ? (
                         updatedItineraries.map((itinerary) => (
-                            <div key={itinerary._id} className="relative overflow-hidden w-full sm:w-[min(100%,40rem)] h-auto border bg-slate-50 dark:bg-black dark:border dark:border-slate-700 rounded-lg shadow-xl p-8 flex flex-col items-start">
-                                <div className="flex items-start w-full">
-                                    <div className="items-center">
-                                        <img src={itinerary.photo} alt={itinerary.name} className="w-28 h-28 rounded-full border-2 border-gray-300 mt-2" />
-                                        <div className="text-center">
-                                            <p className="text-xs text-gray-500">Created by</p>
-                                            <h2 className="font-bold text-lg">{itinerary.name}</h2>
+                            <div
+                                key={itinerary._id}
+                                className="relative overflow-hidden w-[min(100%,34rem)] border bg-slate-50 dark:bg-black dark:border dark:border-slate-700 rounded-lg shadow-xl"
+                            >
+                                {/* Likes Section */}
+                                <div className="absolute left-0 top-0 px-4 py-2 rounded-lg text-2xl gap-2 text-neutral-100 flex bg-neutral-600/50 m-4">
+                                    <HeartIcon
+                                        onClick={() => handleLike(itinerary._id)}
+                                        className={`w-6 h-6 cursor-pointer ${likedItineraries[itinerary._id] ? 'text-red-500' : 'text-gray-400'
+                                            }`}
+                                    />
+                                    <div className="text-lg leading-none text-center">{itinerary.likes}</div>
+                                </div>
+
+                                {/* Background Image */}
+                                <img
+                                    className="w-full aspect-video object-cover"
+                                    src={itinerary.photo}
+                                    alt={itinerary.name}
+                                />
+
+                                {/* Main Content */}
+                                <div className="flex p-4 pb-0 gap-4">
+                                    {/* Creator Section */}
+                                    <div className="flex flex-col items-center justify-center min-w-[5rem]">
+                                        <img
+                                            className="w-14 sm:w-20 aspect-square object-cover rounded-full shadow-md"
+                                            src={itinerary.creatorPhoto}
+                                            alt={`Photo of ${itinerary.creator}`}
+                                        />
+                                        <p className="text-xs font-light mt-1">Created by</p>
+                                        <p className="leading-3 font-bold text-center">
+                                            {itinerary.creator.split(" ")[0]}
+                                        </p>
+                                    </div>
+
+                                    {/* Itinerary Details */}
+                                    <div className="grid w-full">
+                                        <div className="flex flex-wrap w-full text-lg font-medium mt-2 justify-center items-center">
+                                            <h1 className="text-right text-lg font-bold rounded-lg uppercase">{itinerary.name}</h1>
+                                        </div>
+                                        <div className="flex flex-wrap justify-between w-full text-lg font-medium mt-2">
+                                            <div className="flex flex-wrap w-full text-lg font-medium mt-2 justify-center items-center">
+                                                <CurrencyDollarIcon className="w-8 h-8 text-green-500 mr-2" />
+                                                {itinerary.price}
+                                            </div>
+                                            <div className="flex flex-wrap w-full text-lg font-medium mt-2 justify-center items-center">
+                                                <ClockIcon className="w-8 h-8 text-blue-500 mr-2" />
+                                                {itinerary.duration}h
+                                            </div>
+                                        </div>
+                                        {/* Hashtags */}
+                                        <div className="flex w-full justify-center flex-wrap gap-2 mt-4">
+                                            {itinerary.hashtags.map((hashtag, index) => (
+                                                <p key={index} className="rounded-full text-sm bg-blue-700 text-neutral-100 px-3 py-1 shadow-md">
+                                                    {hashtag}
+                                                </p>
+                                            ))}
                                         </div>
                                     </div>
-                                    <div className="ml-auto flex items-center gap-2">
-                                        <HeartIcon onClick={() => handleLike(itinerary._id)} className={`w-8 h-8 cursor-pointer ${likedItineraries[itinerary._id] ? 'text-red-500' : 'text-gray-400'}`} />
-                                        <span className="text-gray-700">{itinerary.likes}</span>
-                                    </div>
                                 </div>
 
-                                <div className="flex w-full justify-center text-3xl text-gray-700 mt-4">
-                                    <CurrencyDollarIcon className="w-8 h-8 text-green-500 mr-2" />
-                                    <span>{itinerary.price}$</span>
-                                </div>
-                                <div className="flex w-full justify-center text-3xl text-gray-700 mt-2">
-                                    <ClockIcon className="w-8 h-8 text-blue-500 mr-2" />
-                                    <span>{itinerary.duration}h</span>
-                                </div>
-
-                                <div className="flex w-full justify-center flex-wrap gap-2 mt-4">
-                                    {itinerary.hashtags.map((hashtag, index) => (
-                                        <p key={index} className="rounded-full text-sm bg-blue-700 text-neutral-100 px-3 py-1 shadow-md">
-                                            {hashtag}
-                                        </p>
-                                    ))}
-                                </div>
-
-                                <div className="flex w-full justify-center flex-wrap gap-2 mt-4">
+                                {/* Toggle Details */}
+                                <div className="flex w-full justify-center flex-wrap gap-2 my-4">
                                     <button onClick={() => toggleShowMore(itinerary._id)} className="bg-blue-700 text-white shadow-lg font-bold rounded-lg p-3 text-center text-xl px-8 hover:bg-blue-600 active:scale-95 transition-all">
                                         {showMore[itinerary._id] ? 'Show Less' : 'View More'}
                                     </button>
